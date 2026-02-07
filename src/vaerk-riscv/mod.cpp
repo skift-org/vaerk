9eb25b8aeebe7e69de29683d2fbdce3fae438171
sleepy-monax <me@smnx.sh>
2026-02-07 12:38:57 +0100

    vaerk-riscv: Added a bunch of definition for riscv.

--- src/vaerk-riscv/mod.cpp
+++ src/vaerk-riscv/mod.cpp
@@ -1,1 +1,111 @@
 export module Vaerk.Riscv;
+
+import Karm.Core;
+
+using namespace Karm;
+
+namespace Vaerk::Riscv {
+
+// MARK: CSR -------------------------------------------------------------------
+
+export enum struct Csr : usize {
+#define CSR(NUM, _, NAME) NAME = NUM,
+#include "defs/csr.inc"
+
+#undef CSR
+};
+
+export usize csrr(Csr csr) {
+    usize tmp;
+    switch (csr) {
+#define CSR(NUM, name, NAME)                                 \
+    case Csr::NAME:                                          \
+        __asm__ __volatile__("csrr %0, " #name : "=r"(tmp)); \
+        break;
+#include "defs/csr.inc"
+
+#undef CSR
+    };
+    return tmp;
+}
+
+export void csrw(Csr csr, usize val) {
+    switch (csr) {
+#define CSR(NUM, name, NAME)                                   \
+    case Csr::NAME:                                            \
+        __asm__ __volatile__("csrw " #name ", %0" ::"r"(val)); \
+        break;
+#include "defs/csr.inc"
+
+#undef CSR
+    };
+}
+
+export usize csrrc(Csr csr, usize mask) {
+    usize tmp;
+    switch (csr) {
+#define CSR(NUM, name, NAME)                           \
+    case Csr::NAME:                                    \
+        __asm__ __volatile__("csrrc %0, " #name ", %1" \
+                             : "=r"(tmp)               \
+                             : "r"(mask));             \
+        break;
+#include "defs/csr.inc"
+
+#undef CSR
+    };
+    return tmp;
+}
+
+export usize csrrs(Csr csr, usize mask) {
+    usize tmp;
+    switch (csr) {
+#define CSR(NUM, name, NAME)                           \
+    case Csr::NAME:                                    \
+        __asm__ __volatile__("csrrs %0, " #name ", %1" \
+                             : "=r"(tmp)               \
+                             : "r"(mask));             \
+        break;
+#include "defs/csr.inc"
+
+#undef CSR
+    };
+    return tmp;
+}
+
+// MARK: Instructions ----------------------------------------------------------
+
+export void unimp() { __asm__ __volatile__("unimp"); }
+
+export void wfi() { __asm__ __volatile__("wfi"); }
+
+export void di() { __asm__ __volatile__("csrci mstatus, 8"); }
+
+export void ei() { __asm__ __volatile__("csrsi mstatus, 8"); }
+
+export void sfenceVma() { __asm__ __volatile__("sfence.vma"); }
+
+struct Ecall {
+    long a0;
+    long a1;
+};
+
+export Ecall ecall(long arg0, long arg1, long arg2, long arg3, long arg4, long arg5, long fid, long eid) {
+    register long a0 __asm__("a0") = arg0;
+    register long a1 __asm__("a1") = arg1;
+    register long a2 __asm__("a2") = arg2;
+    register long a3 __asm__("a3") = arg3;
+    register long a4 __asm__("a4") = arg4;
+    register long a5 __asm__("a5") = arg5;
+    register long a6 __asm__("a6") = fid;
+    register long a7 __asm__("a7") = eid;
+
+    __asm__ __volatile__("ecall"
+                         : "=r"(a0), "=r"(a1)
+                         : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5),
+                           "r"(a6), "r"(a7)
+                         : "memory");
+    return {a0, a1};
+}
+
+} // namespace Vaerk::Riscv
--- src/vaerk-sbi/mod.cpp
+++ src/vaerk-sbi/mod.cpp
@@ -1,36 +1,17 @@
 export module Vaerk.Sbi;
 
 import Karm.Core;
+import Vaerk.Riscv;
 
 using namespace Karm;
 
 namespace Vaerk::Sbi {
 
-struct Ret {
-    long error;
-    long value;
-};
+// MARK: Chapter 5. Legacy Extensions (EIDs #0x00 - #0x0F) ------------------------------------------------------------
 
-export Ret call(long arg0, long arg1, long arg2, long arg3, long arg4, long arg5, long fid, long eid) {
-    register long a0 __asm__("a0") = arg0;
-    register long a1 __asm__("a1") = arg1;
-    register long a2 __asm__("a2") = arg2;
-    register long a3 __asm__("a3") = arg3;
-    register long a4 __asm__("a4") = arg4;
-    register long a5 __asm__("a5") = arg5;
-    register long a6 __asm__("a6") = fid;
-    register long a7 __asm__("a7") = eid;
-
-    __asm__ __volatile__("ecall"
-                         : "=r"(a0), "=r"(a1)
-                         : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5),
-                           "r"(a6), "r"(a7)
-                         : "memory");
-    return (Ret){.error = a0, .value = a1};
-}
-
-export Ret consolePutchar(int ch) {
-    return call(ch, 0, 0, 0, 0, 0, 0, 1);
+// 5.2. Extension: Console Putchar (EID #0x01)
+export usize consolePutchar(int ch) {
+    return Riscv::ecall(ch, 0, 0, 0, 0, 0, 0, 1).a0;
 }
 
 export void consolePuts(Str str) {
