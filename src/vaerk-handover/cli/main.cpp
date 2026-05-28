bc0f9dea9390462ffe7ab9cf44b569703337876e
sleepy-monax <me@smnx.sh>
2026-05-28 10:33:11 +0200

    vaerk-elf: Removed the old elf loader.

--- src/vaerk-handover/cli/main.cpp
+++ src/vaerk-handover/cli/main.cpp
@@ -1,29 +1,28 @@
-import Vaerk.Elf;
+import Karm.Dl.Elf;
 
 #include <karm/entry>
 #include <vaerk-handover/spec.h>
 
 Async::Task<> entryPointAsync(Sys::Env& env, Async::CancellationToken) {
-    if (env.argsLen() == 0) {
+    if (env.argsLen() == 0)
         co_return Error::invalidInput("Usage: handover-dump <elf-file>");
-    }
 
     auto url = Ref::parseUrlOrPath(env[0], env.cwd());
-    auto kernelFile = co_try$(Sys::File::open(url));
-    auto kernelMem = co_try$(Sys::mmap(kernelFile, {.options = Sys::MmapOption::READ}));
-    Vaerk::Elf::Image kernelElf{kernelMem.bytes()};
+    auto file = co_try$(Sys::File::open(url));
+    auto mem = co_try$(Sys::mmap(file, {.options = Sys::MmapOption::READ}));
+    auto elf = Elf::ElfObject<Elf::Elf64LeAbi>{mem.bytes()};
 
-    if (not kernelElf.valid())
+    if (not elf.validate())
         co_return Error::invalidData("kernel is not a valid ELF executable");
 
-    auto requests = co_try$(kernelElf.sectionByName(Handover::REQUEST_SECTION))
-                        .slice<Handover::Request>();
+    auto requests =
+        co_try$(elf.section(Handover::REQUEST_SECTION))
+            .data.cast<Handover::Request>();
 
     Sys::println("Requests:");
-    for (auto const& request : requests) {
+    for (auto const& request : requests)
         Sys::println(" - {}", request.name());
-    }
-
     Sys::println("Kernel is valid");
+
     co_return Ok();
 }
